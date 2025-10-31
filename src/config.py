845999c683b47f45039f8aa36bf70567c36409be
Ali Mert Karaoglu <alimert.karaoglu@hotmail.com>
2025-10-31 18:14:43 +0000

    implemented logging for error and handling and API requets

--- src/config.py
+++ src/config.py
@@ -1,1 +1,12 @@
-BASE_URL = url = "https://api.gdeltproject.org/api/v2/doc/doc"
+from pathlib import Path
+
+# Project paths
+PROJECT_ROOT = Path(__file__).parent.parent
+LOG_DIR = PROJECT_ROOT / "logs"
+LOG_PATH = LOG_DIR / "fetch.logs"
+
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+# API Configuration
+BASE_URL = "https://api.gdeltproject.org/api/v2/doc/doc"
+
