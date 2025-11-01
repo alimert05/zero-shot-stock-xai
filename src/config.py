8953ee40e1f5b66c71519097003285e1b218f532
Ali Mert Karaoglu <alimert.karaoglu@hotmail.com>
2025-11-01 06:05:14 +0000

    implemented article save in temp file

--- src/config.py
+++ src/config.py
@@ -4,7 +4,10 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent.parent
 LOG_DIR = PROJECT_ROOT / "logs"
 LOG_PATH = LOG_DIR / "fetch.logs"
+DATA_PATH = PROJECT_ROOT / "data"
+TEMP_PATH = DATA_PATH / "temp"
 
+TEMP_PATH.mkdir(parents=True, exist_ok=True)
 LOG_DIR.mkdir(parents=True, exist_ok=True)
 
 # API Configuration
