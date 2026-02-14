4bab64825c07d4116e247c397f5c69a7dceb297c
Ali Mert Karaoglu <alimert.karaoglu@hotmail.com>
2026-02-14 14:37:09 +0000

    fetcher changed from gdeltdoc to finnhub

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Project paths
@@ -19,7 +20,7 @@ LOG_DIR.mkdir(parents=True, exist_ok=True)
 REQUEST_TIMEOUT_LIMIT = 30
 
 # API Configuration
-BASE_URL = "https://api.gdeltproject.org/api/v2/doc/doc"
+FINNHUB_API_KEY = "d5rvt19r01qq2th0b8sgd5rvt19r01qq2th0b8t0"
 
 WEIGHT_COMBINE_METHOD = "geometric"
 
@@ -34,33 +35,17 @@ HORIZON_IMPORTANCE = 0.6
 SENTIMENT_DEVICE = 0 
 
 # finbert config
-# SENTIMENT_MODEL = "ProsusAI/finbert"
-# SENTIMENT_MAX_LENGTH = 512  
+SENTIMENT_MODEL = "ProsusAI/finbert"
+SENTIMENT_MAX_LENGTH = 512  
 
 #fingpt config
-SENTIMENT_MODEL = "fingpt"
-FINGPT_BASE_MODEL = "NousResearch/Llama-2-13b-hf"
-FINGPT_LORA_MODEL = "FinGPT/fingpt-sentiment_llama2-13b_lora"
-FINGPT_LOAD_IN_8BIT = True
+# SENTIMENT_MODEL = "fingpt"
+# FINGPT_BASE_MODEL = "NousResearch/Llama-2-13b-hf"
+# FINGPT_LORA_MODEL = "FinGPT/fingpt-sentiment_llama2-13b_lora"
+# FINGPT_LOAD_IN_8BIT = True
 
 # zero-shot config
 # SENTIMENT_MODEL = "zero-shot"
 # MODEL_NAME = "facebook/bart-large-mnli" # facebook bart mnli
 # MODEL_NAME = "roberta-large-mnli" # facebookAI roberta mnli
 MODEL_NAME = "microsoft/deberta-large-mnli" # microsoft deberta large mnli
-
-THEMES = [
-    "ECON_STOCKMARKET",
-    "ECON_CENTRALBANK",
-    # "ECON_INTEREST_RATES",
-    # "EPU_CATS_FINANCIAL_REGULATION",
-    # "EPU_ECONOMY",
-
-    # "ECON_INFLATION",
-    # "ECON_WORLDCURRENCIES",
-    # "ECON_CURRENCY_EXCHANGE_RATE",
-    # "EPU_POLICY",
-    # "EPU_CATS_MONETARY_POLICY",
-    # "ECON_TAXATION",
-    # "ECON_IPO",
-]
