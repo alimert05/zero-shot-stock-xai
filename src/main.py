2234400fe8924f2a6e83b6c9281ebf6daf9580cb
Ali Mert Karaoglu <alimert.karaoglu@hotmail.com>
2026-02-27 01:37:57 +0000

    final polishment for impact horizon in xai

--- src/main.py
+++ src/main.py
@@ -9,32 +9,32 @@ from xai import run_xai
 
 def main() -> None:
 
-    fetcher = Fetcher()
-    fetcher.run_fetcher()
+    # fetcher = Fetcher()
+    # fetcher.run_fetcher()
 
-    prediction_result = None
+    # prediction_result = None
 
-    if SENTIMENT_MODEL == "fingpt":
-        prediction_result = run_fingpt(articles_json_path=str(JSON_PATH), output_path=str(FINGPT_PREDS))
-    elif SENTIMENT_MODEL == "ProsusAI/finbert":
-        prediction_result = run_finbert(articles_json_path=str(JSON_PATH), output_path=str(FINBERT_PREDS))
-    elif SENTIMENT_MODEL == "zero-shot":
-        if MODEL_NAME == "facebook/bart-large-mnli":
-            path = "BART Large MNLI"
-        if MODEL_NAME == "roberta-large-mnli":
-            path = "RoBERTa Large MNLI"
-        if MODEL_NAME == "microsoft/deberta-large-mnli":
-            path = "DeBERTa Large MNLI"
-        prediction_result = run_zero_shot(articles_json_path=str(JSON_PATH), output_path=str(ZEROSHOT_PREDS))
+    # if SENTIMENT_MODEL == "fingpt":
+    #     prediction_result = run_fingpt(articles_json_path=str(JSON_PATH), output_path=str(FINGPT_PREDS))
+    # elif SENTIMENT_MODEL == "ProsusAI/finbert":
+    #     prediction_result = run_finbert(articles_json_path=str(JSON_PATH), output_path=str(FINBERT_PREDS))
+    # elif SENTIMENT_MODEL == "zero-shot":
+    #     if MODEL_NAME == "facebook/bart-large-mnli":
+    #         path = "BART Large MNLI"
+    #     if MODEL_NAME == "roberta-large-mnli":
+    #         path = "RoBERTa Large MNLI"
+    #     if MODEL_NAME == "microsoft/deberta-large-mnli":
+    #         path = "DeBERTa Large MNLI"
+    #     prediction_result = run_zero_shot(articles_json_path=str(JSON_PATH), output_path=str(ZEROSHOT_PREDS))
 
-    if XAI_ENABLED and prediction_result is not None:
-        run_xai(
-            prediction_result=prediction_result,
-            articles_json_path=str(JSON_PATH),
-            output_path=str(XAI_OUTPUT_PATH),
-        )
+    # if XAI_ENABLED and prediction_result is not None:
+    #     run_xai(
+    #         prediction_result=prediction_result,
+    #         articles_json_path=str(JSON_PATH),
+    #         output_path=str(XAI_OUTPUT_PATH),
+    #     )
 
-    # run_backtest()
+    run_backtest()
 
 if __name__ == "__main__":
     main()
